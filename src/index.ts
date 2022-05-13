fa873ccdbedcfc590d1bc026c577513ccb2843c4
Edgars Burtnieks <edburtnieks@pm.me>
2022-05-13 15:01:00 +0300

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,32 @@
 import type { AstroIntegration, AstroConfig } from "astro";
-import { readFileSync, writeFileSync, existsSync } from "fs";
-import {
-    DIRECTORY_PAGES,
-    DIRECTORY_LAYOUTS,
-    NAME,
-    TEMPLATE_PAGE_INTEGRATIONS,
-    TEMPLATE_LAYOUT_BASE,
-} from "./constants";
-import {
-    copyLayoutTemplate,
-    copyPageTemplate,
-    createDirectoryStructure,
-} from "./utils";
+import * as constants from "./constants";
+import * as utils from "./utils";
 
 export default function createPlugin(): AstroIntegration {
     return {
-        name: NAME,
+        name: constants.NAME,
         hooks: {
             "astro:config:setup": ({ config }: { config: AstroConfig }) => {
                 // Assign constants
                 const path = config.srcDir.pathname;
-                const pagesDir = DIRECTORY_PAGES(path);
-                const layoutsDir = DIRECTORY_LAYOUTS(path);
+                const pagesDir = constants.DIRECTORY_PAGES(path);
+                const layoutsDir = constants.DIRECTORY_LAYOUTS(path);
 
                 // Create necessary directories
-                createDirectoryStructure(pagesDir);
-                createDirectoryStructure(layoutsDir);
+                utils.createDirectoryStructure(pagesDir);
+                utils.createDirectoryStructure(layoutsDir);
 
                 // Copy layouts
-                copyLayoutTemplate(path, TEMPLATE_LAYOUT_BASE);
+                utils.copyLayoutTemplate(path, constants.TEMPLATE_LAYOUT_BASE);
 
                 // Copy pages
-                copyPageTemplate(path, TEMPLATE_PAGE_INTEGRATIONS);
+                utils.copyPageTemplate(
+                    path,
+                    constants.TEMPLATE_PAGE_INTEGRATIONS
+                );
             },
         },
     };
 }
+
+export { constants, utils };
