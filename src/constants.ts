0a09d3fefb5a5f17062dcabad1bcbc54aa37540b
Edgars Burtnieks <edburtnieks@pm.me>
2022-05-13 15:58:03 +0300

    Fix paths

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const NAME = "astro-dashboard-ui";
 
-export const DIRECTORY_PAGES = (path: string) => `${path}/pages/${NAME}`;
-export const DIRECTORY_LAYOUTS = (path: string) => `${path}/layouts/${NAME}`;
+export const DIRECTORY_PAGES = (path: string) => `${path}pages/${NAME}`;
+export const DIRECTORY_LAYOUTS = (path: string) => `${path}layouts/${NAME}`;
 
 export const TEMPLATE_LAYOUT_BASE = "BaseLayout.astro";
 export const TEMPLATE_PAGE_INTEGRATIONS = "integrations.astro";
--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,28 @@ export default function createPlugin(): AstroIntegration {
         hooks: {
             "astro:config:setup": ({ config }: { config: AstroConfig }) => {
                 // Assign constants
-                const path = config.srcDir.pathname;
-                const pagesDir = constants.DIRECTORY_PAGES(path);
-                const layoutsDir = constants.DIRECTORY_LAYOUTS(path);
+                const rootPath = config.root.pathname;
+                const srcPath = config.srcDir.pathname;
+                const pagesDir = constants.DIRECTORY_PAGES(srcPath);
+                const layoutsDir = constants.DIRECTORY_LAYOUTS(srcPath);
 
                 // Create necessary directories
                 utils.createDirectoryStructure(pagesDir);
                 utils.createDirectoryStructure(layoutsDir);
 
                 // Copy layouts
-                utils.copyLayoutTemplate(path, constants.TEMPLATE_LAYOUT_BASE);
+                utils.copyLayoutTemplate({
+                    rootPath,
+                    srcPath,
+                    file: constants.TEMPLATE_LAYOUT_BASE,
+                });
 
                 // Copy pages
-                utils.copyPageTemplate(
-                    path,
-                    constants.TEMPLATE_PAGE_INTEGRATIONS
-                );
+                utils.copyPageTemplate({
+                    rootPath,
+                    srcPath,
+                    file: constants.TEMPLATE_PAGE_INTEGRATIONS,
+                });
             },
         },
     };
